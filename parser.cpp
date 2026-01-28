079237185999c8f7158f92ba6cbbefc6acfd54c0
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-01-28 11:13:09 -0500

    feat: add parser.cpp

---
+++ parser.cpp
@@ -0,0 +1,18 @@
+// Fountain Parser
+// Parses .fountain file into HTML markup
+
+void trim_leading_newlines() {
+  // Normalize input by triming all leading newlines
+};
+
+void find_first_blank_line_range() {
+  // Determines where last line of title page is (if exists)
+};
+
+void contains_titlepage() {
+  // Checks if content at top of document is a title page
+};
+
+void strip_boneyard_blocks() {
+  // Remove all boneyard blocks from input (not included in output)
+};
